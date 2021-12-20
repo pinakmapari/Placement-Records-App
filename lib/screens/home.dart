647b4e2d715dca90057ba55ca67182c4048cb8dd
sejalpekam <sejalpekam@gmail.com>
2021-12-20 15:03:58 +0530

    Student home changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,7 +13,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController yearController = TextEditingController();
-
+  List cityNames = [
+    "Pune",
+    "Mumbai",
+    "Delhi",
+    "Bengluru",
+    "Hyderabad",
+    "Kolkata"
+  ];
+  late String cityName;
+  late int year;
+  late double min, max;
   String search = '';
   @override
   Widget build(BuildContext context) {
@@ -24,108 +34,166 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black54,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black54,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xffE3EEFF),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xffE3EEFF),
+                        ),
+                        borderRadius: BorderRadius.circular(40)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xffE3EEFF),
+                        ),
+                        borderRadius: BorderRadius.circular(40)),
+                    hintText: "Search",
                   ),
-                  filled: true,
-                  fillColor: Color(0xffE3EEFF),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xffE3EEFF),
-                      ),
-                      borderRadius: BorderRadius.circular(40)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xffE3EEFF),
-                      ),
-                      borderRadius: BorderRadius.circular(40)),
-                  hintText: "Search",
+                  onChanged: (value) => search = value,
                 ),
-                onChanged: (value) => search = value,
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text('Enter Package Range: ',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                ],
+              SizedBox(
+                height: 40,
               ),
-            ),
-            RangeSliderLabelWidget(),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Enter City: ',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  CityFilter(),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Text(
-                    'Enter Year: ',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      controller: yearController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0xffE3EEFF),
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(40.0))),
-                        //labelText: 'Academic Year',
-                        hintText: 'Enter year',
+              Container(
+                child: Column(
+                  children: [
+                    Text('Enter Package Range: ',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xffE3EEFF),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0))),
+                          hintText: 'Min LPA',
+                        ),
+                        onChanged: (value) =>
+                            setState(() => min = double.parse(value)),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    height: 60,
-                    child: ElevatedButton(
-                      
-                      child: Text("     Submit    ", style: TextStyle(fontSize: 18),),
-                      onPressed: () => print("Submit pressed"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue[400],
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xffE3EEFF),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0))),
+                          hintText: 'Max LPA',
+                        ),
+                        onChanged: (value) =>
+                            setState(() => max = double.parse(value)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //RangeSliderLabelWidget(),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Enter City: ',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    //CityFilter(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xffE3EEFF),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0))),
+                          hintText: 'Enter city',
+                        ),
+                        onChanged: (value) => setState(() => cityName = value),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      'Enter Year: ',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: yearController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xffE3EEFF),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0))),
+                          //labelText: 'Academic Year',
+                          hintText: 'Enter year',
+                        ),
+                        onChanged: (value) =>
+                            setState(() => year = int.parse(value)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 60,
+                      child: ElevatedButton(
+                        child: Text(
+                          "     Submit    ",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        onPressed: () => print("Submit pressed"),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue[400],
+                          onPrimary: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
